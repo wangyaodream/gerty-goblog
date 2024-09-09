7fdb083525b0e785cbe6a189b86b43466bf591c1
wangyao <wangyao.dream@hotmail.com>
2024-09-09 23:22:04 +0800

    add init config moudile

---
+++ cmd/init_config.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	envVars := []string{
+		"DB_HOST",
+		"DB_USER",
+		"DB_PASSWORD",
+		"DB_PORT",
+		"DB_TYPE",
+	}
+	file, err := os.Create(".env")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	for _, key := range envVars {
+		// 每个环境变量后面跟一个等号和一对空的双引号
+		file.WriteString(fmt.Sprintf("%s=\"\"\n", key))
+	}
+
+	fmt.Println("file created successfully")
+}

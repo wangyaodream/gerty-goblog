874c2e072f614732bc7eed5b2a5009743f9e64bf
wangyao <wangyao.dream@hotmail.com>
2024-09-20 17:06:37 +0800

    add config file app

---
+++ config/app.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"github.com/wangyaodream/gerty-goblog/pkg/config"
+)
+
+func init() {
+	config.Add("app", config.StrMap{
+		"name":  config.Env("APP_NAME", "GoBlog"),
+		"env":   config.Env("APP_ENV", "production"),
+		"debug": config.Env("APP_DEBUG", false),
+		"port":  config.Env("APP_PORT", "3000"),
+		"key":   config.Env("APP_KEY", "33446a9dcf9ea060a0a6532b166da32f304af0de"),
+	})
+}

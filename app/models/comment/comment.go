cdd8de1b810a6967138e2d8e913e6d1480a8f104
wangyao <wangyao.dream@hotmail.com>
2024-10-10 23:15:36 +0800

    add article foregin key for comment

--- app/models/comment/comment.go
+++ app/models/comment/comment.go
@@ -2,13 +2,16 @@ package comment
 
 import (
 	"github.com/wangyaodream/gerty-goblog/app/models"
+	"github.com/wangyaodream/gerty-goblog/app/models/article"
 	"github.com/wangyaodream/gerty-goblog/app/models/user"
 )
 
 type Comment struct {
 	models.BaseModel
 
-	Content string `gorm:"type:longtext;not null;" valid:"content"`
-	UserID  uint64 `gorm:"not null;index"`
-	User    user.User
+	Content   string `gorm:"type:longtext;not null;" valid:"content"`
+	UserID    uint64 `gorm:"not null;index"`
+	User      user.User
+	ArticleID uint64 `gorm:"not null;index"`
+	Article   article.Article
 }
